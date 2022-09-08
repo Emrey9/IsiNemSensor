90896f692cc19b05b9fd2b9eaf7a50b49a1bec74
Emre YILMAZ <218EE2190@isik.edu.tr>
2022-09-08 03:29:57 +0300

    Add project files.

---
+++ IsiNemSensor/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace IsiNemSensor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(767, 229);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PortAc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PortKapa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ IsiNemSensor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IsiNemSensor
+{
+    public partial class Form1 : Form
+    {
+        String sicaklik;
+        String nem;
+        String cihaz_id;
+        String sistem_id;
+        string RxString;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+
+            
+            this.Invoke(new EventHandler(DisplayText));
+            
+        }
+
+        private void rest_yaz(String sicaklik_, String nem_, String cihaz_id_, String sistem_id_)
+        {
+            try { 
+                var client = new RestClient("http://193.140.154.92:3000/sicaklikAl");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{""sicaklik"" : """+sicaklik_+@""",""nem"" : """+nem_+@""",""cihaz_id"" :"""+cihaz_id_+@""",""sistem_id"" :"""+sistem_id_+@"""}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+        
+        
+        private void DisplayText(object sender, EventArgs e)
+        {
+            RxString = serialPort1.ReadExisting();
+            string[] words = RxString.Split(',');
+            int i = 0;
+            foreach (string word in words)
+            {
+               // richTextBox1.AppendText(word+Environment.NewLine);
+                if (i == 0 && !words[0].Equals(""))
+                {
+                    richTextBox1.AppendText("Nem: " + words[0]+ Environment.NewLine);
+                    nem = words[0];
+                    i++;
+                }
+                else if (i == 1)
+                {
+                    richTextBox1.AppendText("Isı: " + words[1]+ Environment.NewLine);
+                    sicaklik = words[1];
+                }
+            }
+            try
+            {
+
+                if (i == 1) { rest_yaz(sicaklik, nem, "25", "14"); }
+            }
+           catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+           // richTextBox1.AppendText("Nem: "+words[0]+ " Isı: "+ words[1] + Environment.NewLine);
+           // Console.WriteLine(RxString);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+                try
+                {
+                    serialPort1.Open();
+                    Console.WriteLine("opened");
+
+                }
+
+                catch (Exception oex)
+                {
+                    MessageBox.Show(oex.ToString());
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close(); 
+            Console.WriteLine("closed");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+    }
+}
